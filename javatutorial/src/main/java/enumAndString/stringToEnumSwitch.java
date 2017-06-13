20e668d39d9c55bc1de0adcd9840fafb2dfffe41
zhaoyi <zoeyyang_bupt@163.com>
2017-06-13 13:37:33 +0800

    以枚举形式存在的一组数据 switch匹配的方法

---
+++ javatutorial/src/main/java/enumAndString/stringToEnumSwitch.java
@@ -0,0 +1,29 @@
+package enumAndString;
+
+/**
+ * Created by zhaoyi on 17-6-13.
+ * 要先新建一个枚举类型的值 并赋它 string的值
+ */
+enum EnumExample
+{
+    Teamreq,Matreq;
+}
+public class stringToEnumSwitch {
+    public static void main(String[] argc)
+    {
+        String str = "Matreq";
+        EnumExample test = EnumExample.valueOf(str);
+        switch (test)
+        {
+            case Matreq:
+                System.out.println(EnumExample.Matreq.name());
+                break;
+            case Teamreq:
+                System.out.println(EnumExample.Teamreq.name());
+                break;
+            default:
+                break;
+        }
+
+    }
+}
