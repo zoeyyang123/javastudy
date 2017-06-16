f0bc65c62a3dab817c1fc58093e93f967cfdb84e
zhaoyi <zoeyyang_bupt@163.com>
2017-06-16 16:35:10 +0800

    一些例程

---
+++ javatutorial/src/main/java/enumAndString/numToEnum.java
@@ -0,0 +1,60 @@
+package enumAndString;
+
+/**
+ * Created by zhaoyi on 17-6-15.
+ */
+enum BussType {
+
+    UNDERWRITE("02"),
+    TOCANCEL("31"),
+    CLAIM("15"),
+    PRODUCTRECOMMEND("47");
+
+
+    private String code;
+
+    BussType(String code) {
+        this.code = code;
+    }
+    public String toValue() {
+        return code;
+    }
+    public static BussType getEnumByCode(String code){
+        if (code.equals(UNDERWRITE.toValue())){
+            return UNDERWRITE;
+        }else if (code.equals(TOCANCEL.toValue())){
+            return TOCANCEL;
+        }else if (code.equals(CLAIM.toValue())){
+            return CLAIM;
+        }else if (code.equals(PRODUCTRECOMMEND.toValue())){
+            return PRODUCTRECOMMEND;
+        }else {
+            return null;
+        }
+    }
+
+}
+public class numToEnum {
+    public static void main(String[] args) {
+        String temp = "02";
+        BussType bussType = BussType.getEnumByCode(temp);
+        switch (bussType)
+        {
+            case UNDERWRITE:
+                System.out.println(BussType.UNDERWRITE.name());
+                break;
+            case TOCANCEL:
+                System.out.println(BussType.TOCANCEL.name());
+                break;
+            case CLAIM:
+                System.out.println(BussType.CLAIM.name());
+                break;
+            case PRODUCTRECOMMEND:
+                System.out.println(BussType.PRODUCTRECOMMEND.name());
+                break;
+            default:
+                break;
+        }
+    }
+
+}
---
+++ javatutorial/src/main/java/longToString/LongToString.java
@@ -0,0 +1,15 @@
+package longToString;
+
+/**
+ * Created by zhaoyi on 17-6-14.
+ */
+public class LongToString {
+    public final static long ss = 1234512312;
+
+    public static void main(String[] args) {
+        System.out.println(""+ss);
+        System.out.println(String.valueOf(ss));
+        System.out.println(String.valueOf(System.currentTimeMillis()));
+    }
+
+}
