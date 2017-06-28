1e8d71e2b2d537f118eec2e6a80a572797816ba7
zhaoyi <zoeyyang_bupt@163.com>
2017-06-28 15:51:59 +0800

    LeetCode No.9

---
+++ javatutorial/src/main/java/leetCode/PalindromeNumber.java
@@ -0,0 +1,36 @@
+package leetCode;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zhaoyi on 17-6-28.
+ */
+public class PalindromeNumber {
+
+
+    public static boolean isPalindrome(int x) {
+        if (x<0){
+            return false;
+        }else if(x==0){
+            return true;
+        }else {
+            ArrayList<Integer> temp = new ArrayList<Integer>();
+            int j =0;
+            while (x!= 0){
+                j=x%10;
+                temp.add(x%10);
+                x=(x-j)/10;
+            }
+            int t = temp.size();
+            for (int i = 0; i < t; i++) {
+                if (temp.get(i)!=temp.get(t-1-i))
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(111232111));
+    }
+}
