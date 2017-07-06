e372d1e0eb7f5e612739f9b11a7ed2785dc07719
zhaoyi <zoeyyang_bupt@163.com>
2017-07-06 09:47:04 +0800

    LeetCode No.14

---
+++ javatutorial/src/main/java/leetCode/LongestCommonPrefix.java
@@ -0,0 +1,49 @@
+package leetCode;
+
+/**
+ * Created by zhaoyi on 17-6-29.
+ */
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuffer result = new StringBuffer();
+        int strsLength = strs.length;
+        if (strsLength==0)
+            return "";
+        else if (strsLength==1)
+            return result.append(strs[0]).toString();
+        else {
+            int maxLength = strs[0].length();
+            for (int i = 1; i < strsLength; i++) {
+                int temp = strs[i].length();
+                maxLength = maxLength>temp?temp:maxLength;
+            }
+            boolean flag1 = false;
+            for (int j = 0; j < maxLength; j++) {
+                Character temp = strs[0].charAt(j);
+                for (int k = 1; k < strsLength; k++) {
+                    if (!temp.equals(strs[k].charAt(j))){
+                        flag1 = true;
+                        break;
+                    }
+                }
+                if (flag1) {
+                    break;
+                }else {
+                    result.append(temp);
+                }
+            }
+            return result.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] strs1 = new String[3];
+        strs1[0] = "babc";
+        strs1[1] = "cabcdefg";
+        strs1[2] = "abxyz";
+        String[] strs2 = new String[1];
+        strs2[0] = "";
+        String[] strs3 = new String[0];
+        System.out.println("结果："+longestCommonPrefix(strs3));
+    }
+}
