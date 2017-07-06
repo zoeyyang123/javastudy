e4604406ed84cd817a03933098a93b3b33925532
zhaoyi <zoeyyang_bupt@163.com>
2017-07-06 09:46:23 +0800

    递归练习

---
+++ javatutorial/src/main/java/tenStages/TenStages.java
@@ -0,0 +1,22 @@
+package tenStages;
+
+/**
+ * Created by zhaoyi on 17-7-3.
+ */
+public class TenStages {
+    private static int total(Integer n){
+        if (n<=0)
+            return 0;
+        else if (n==1)
+            return 1;
+        else if (n==2){
+            return 2;
+        }else {
+            return total(n-2)+total(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(total(100));
+    }
+}
