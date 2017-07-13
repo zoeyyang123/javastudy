ffe8e1b4d83f0f33b9446015af270129ed89b9e8
zhaoyi <zoeyyang_bupt@163.com>
2017-07-13 11:37:40 +0800

    LeetCode No.83. Remove Duplicates from Sorted List.用正向循环比用递归快很多

---
+++ javatutorial/src/main/java/leetcode/easy/ClimbingStairs.java
@@ -0,0 +1,39 @@
+package leetcode.easy;
+
+/**
+ * Created by zhaoyi on 17-7-13.
+ */
+public class ClimbingStairs {
+    /*private static int climbStairs(int n){
+        if (n<=0)
+            return 0;
+        else if (n==1)
+            return 1;
+        else if (n==2){
+            return 2;
+        }else {
+            return climbStairs(n-2)+climbStairs(n-1);
+        }
+    }*/
+    private static int climbStairs(int n){
+        if (n<=0)
+            return 0;
+        else if (n==1)
+            return 1;
+        else if (n==2){
+            return 2;
+        }else {
+            int[] result = new  int[n];
+            result[0]=1;
+            result[1]=2;
+            for (int i = 2; i < n; i++) {
+                result[i]=result[i-1]+result[i-2];
+            }
+            return result[n-1];
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(45));
+    }
+}
