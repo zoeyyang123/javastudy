42655ee9ed577ba27eb1a2eaa8ef8f66a3f620c6
zhaoyi <zoeyyang_bupt@163.com>
2017-11-13 10:59:38 +0800

    借助CGLib使方法去内存溢出异常

---
+++ javatutorial/src/main/java/jvm/JavaMethodAreaOOM.java
@@ -0,0 +1,33 @@
+package jvm;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by zhaoyi on 17-11-13.
+ * VM Args： -XX:MetaspaceSize=10M -XX:MaxMetaspaceSize=10M
+ * @author zhaoyi
+ */
+public class JavaMethodAreaOOM {
+
+    public static void main(String[] args) {
+        while (true) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                    return proxy.invokeSuper(obj, args);
+                }
+            });
+            enhancer.create();
+        }
+    }
+
+    static class OOMObject {
+
+    }
+}
