fe8e8d086620e8670283389675a8cd0684847558
zhaoyi <zoeyyang_bupt@163.com>
2018-01-19 10:18:14 +0800

    添加一个scala练习工程

---
+++ scalatutorial/src/main/scala/littleTest/Test1.scala
@@ -0,0 +1,14 @@
+package littleTest
+
+/**
+  * Created by zhaoyi on 18-1-17.
+  */
+class Test1 {
+
+}
+object Test1{
+  def main(args: Array[String]): Unit = {
+    val str = "hello scala!"
+    println(str)
+  }
+}
