5a6d50ff26d3429c4b47a7cf7590817d9a6d8874
zhaoyi <zoeyyang_bupt@163.com>
2018-07-10 17:31:46 +0800

    复习守护线程

--- javatutorial/src/main/java/thread/chapter2/DeamonDemo.java
+++ javatutorial/src/main/java/thread/chapter2/DeamonDemo.java
@@ -12,9 +12,9 @@ public class DeamonDemo {
         @Override
         public void run() {
             while (true){
-                System.out.println("I'm alive");
+                System.out.println(Thread.currentThread().getId()+"I'm alive");
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(1000);
                 }catch (InterruptedException e){
                     e.printStackTrace();
                 }
@@ -23,10 +23,14 @@ public class DeamonDemo {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t = new DaemonT();
-        //t.setDaemon(true);
-        t.start();
+        Thread t1 = new DaemonT();
+        t1.setDaemon(true);
+        t1.start();
+        Thread t2 = new DaemonT();
+        t2.start();
 
         Thread.sleep(2000);
+        t2.stop();//如果没有此行 则主线程和两个用户线程中，主线程结束，但是用户线程t2并没有结束，则t1作为守护线程，不会结束
+
     }
 }
